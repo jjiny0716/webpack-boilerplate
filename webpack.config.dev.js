05d2c03321932f34e70fb407ebb119193c98d10e
jjiny <jjiny3773@naver.com>
2022-09-26 19:29:38 +0900

    feat: 이미지 파일 복사
    - CopyWebpackPlugin을 이용해 이미지등의 정적 리소스를 빌드 폴더로 복사
    - DefinePlugin을 이용해 개발 환경을 체크할 수 있는 변수 등록
    - webpack-dev-server관련 코드 작성

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,36 +1,48 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+const mode = "development";
 
 module.exports = {
-  mode: "development",
+  mode,
   devtool: "inline-source-map",
   entry: "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, "build"),
     filename: "main.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./resource"),
+      publicPath: "/resource",
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
     ],
-  },  
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new webpack.DefinePlugin({
+      IS_DEV: JSON.stringify(mode === "development"),
     }),
   ],
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,17 @@
+const webpack = require("webpack");
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const mode = "production";
 
 module.exports = {
-  mode: "production",
+  mode,
   entry: "./src/js/index.js",
   output: {
     path: path.resolve(__dirname, '../BE/public'),
     filename: "main.js",
+    assetModuleFilename: 'resource/image/[name].[ext]',
   },
   module: {
     rules: [
@@ -25,11 +30,24 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+        type: 'asset/resource',
+        exclude: /node_modules/,
+      },
     ],
   },  
   plugins: [
     new HTMLWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'resource', to: "resource" } // static폴더로부터
+      ]
+    }),
+    new webpack.DefinePlugin({
+      IS_DEV: JSON.stringify(mode === "development"),
     }),
   ],
 };
