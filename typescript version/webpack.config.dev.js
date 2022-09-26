da600dc56b46e0074a297d446062e53dc693e24e
jjiny <jjiny3773@naver.com>
2022-09-26 23:29:03 +0900

    feat: 타입스크립트 버전 추가

---
+++ typescript version/webpack.config.dev.js
@@ -0,0 +1,53 @@
+const webpack = require("webpack");
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+const mode = "development";
+
+module.exports = {
+  mode,
+  devtool: "inline-source-map",
+  entry: "./src/typescript/index.ts",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./resource"),
+      publicPath: "/resource",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.DefinePlugin({
+      IS_DEV: JSON.stringify(mode === "development"),
+    }),
+  ],
+};
---
+++ typescript version/webpack.config.prod.js
@@ -0,0 +1,58 @@
+const webpack = require("webpack");
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const mode = "production";
+
+module.exports = {
+  mode,
+  entry: "./src/typescript/index.ts",
+  output: {
+    path: path.resolve(__dirname, '../BE/public'),
+    filename: "main.js",
+    assetModuleFilename: 'resource/image/[name].[ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+        type: 'asset/resource',
+        exclude: /node_modules/,
+      },
+    ],
+  },  
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'resource', to: "resource" } // static폴더로부터
+      ]
+    }),
+    new webpack.DefinePlugin({
+      IS_DEV: JSON.stringify(mode === "development"),
+    }),
+  ],
+};
